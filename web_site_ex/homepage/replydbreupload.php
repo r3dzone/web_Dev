52ea7f86962879b0c53a968a940b658e46e72a69
r3dzone <coolitop123@gmail.com>
2020-05-18 07:53:55 +0000

    change mysql func

--- web_site_ex/homepage/replydbreupload.php
+++ web_site_ex/homepage/replydbreupload.php
@@ -4,42 +4,39 @@ $id =$_SESSION['id'];
 $nick =$_SESSION['nick'];
 
 echo $_POST["reply"];
-$kn = $_POST["kn"];
+$idx = $_POST["idx"];
 $replyn = $_POST["replyn"];
 
 $err = 0;
 
 if($_POST["reply"]==""){
 echo "<script>alert('empty!!')</script>";
-echo '<script>location.href="http:///~unknown/homepage/read.php?kn='.$kn.'"</script>';
+echo '<script>location.href="/read.php?idx='.$idx.'"</script>';
 $err = 3;
 }
 
 echo "<script>alert('success upload!!')</script>";
 $dbHost = 'localhost';
-$dbId = 'unknown';
-$dbPw = 'redzone';
-$dbName = 'unknown';
-$conn = mysql_connect($dbHost,$dbId,$dbPw);
-$a = mysql_select_db($dbName,$conn);
+$dbId = '';
+$dbPw = '';
+$dbName = '';
+$conn = mysqli_connect($dbHost,$dbId,$dbPw,$dbName);
 
-mysql_query("set session character_set_connection=utf8;");
-
-mysql_query("set session character_set_results=utf8;");
-
-mysql_query("set session character_set_client=utf8;");
+mysqli_query($conn,"set session character_set_connection=utf8;");
+mysqli_query($conn,"set session character_set_results=utf8;");
+mysqli_query($conn,"set session character_set_client=utf8;");
 
 if(isset($_POST["reply"])&& $err !=3 ){
 	
-$query = "update reply set secret = '".$_POST["secret"]."' where replyn =".$replyn;
-$result = mysql_query($query, $conn);
+$query = "update reply set secret = '".$_POST["secret"]."' where replyn =".$replyn.";";
+$result = mysqli_query($conn,$query);
 
-$query = "update reply set reply = '".$_POST["reply"]."' where replyn =".$replyn;
-$result = mysql_query($query, $conn);
+$query = "update reply set reply = '".$_POST["reply"]."' where replyn =".$replyn.";";
+$result = mysqli_query($conn,$query);
 
 }
-mysql_close($conn);
+mysqli_close($conn);
 
-echo '<script>location.href="http:///~unknown/homepage/read.php?kn='.$kn.'"</script>';
+echo '<script>location.href="/read.php?idx='.$idx.'"</script>';
 
 	?>
