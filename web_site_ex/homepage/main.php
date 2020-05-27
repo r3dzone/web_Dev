d162b641175219c9ae87459ebf403976e4c7dc6d
r3dzone <coolitop123@gmail.com>
2020-05-27 10:09:48 +0000

    add header section

--- web_site_ex/homepage/main.php
+++ web_site_ex/homepage/main.php
@@ -5,22 +5,24 @@
 		<link rel="stylesheet" type="text/css" href="/css/style.css">
 		<title>정신과 시간의 방</title>
 	</head>
-	<body>
-	   <h1 font-family="ITCBLKAD"><a href="/main.php"><img src="/images/logo.png" alt="정신과시간의방"></a>
-<nav id="menubar" >
-		<ul>
-			<li><a class="sel" href="/freeboard.php">FreeBoard</a></li>
-			<li>|</li>
-			<li><a class="sel" href="/mypage.php">MyPage</a></li>
-			<li>|</li>
-			<li><a class="sel" href="/chat.php">Chat</a></li>
-			<li>|</li>
-			<li><a class="sel" href="">Secret</a></li>
-			<li>|</li>
-			<li><a class="sel" href="">Secret</a></li>
-		</ul>
-	</nav>
-<ul id = "menu">
+	<header>
+		<h1 font-family="ITCBLKAD"><a href="/main.php"><img src="/images/logo.png" alt="정신과시간의방"></a>
+		<nav id="menubar" >
+			<ul>
+				<li><a class="sel" href="/freeboard.php">FreeBoard</a></li>
+				<li>|</li>
+				<li><a class="sel" href="/mypage.php">MyPage</a></li>
+				<li>|</li>
+				<li><a class="sel" href="/chat.php">Chat</a></li>
+				<li>|</li>
+				<li><a class="sel" href="">Secret</a></li>
+				<li>|</li>
+				<li><a class="sel" href="">Secret</a></li>
+			</ul>
+		</nav>
+	</header>
+<body>	
+	<ul id = "menu">
 <?php
  session_start();
 if(isset($_SESSION['id'])){
@@ -33,8 +35,8 @@ echo '<li><a href="http://hypertime.tk/signup.php">회원가입</a></li>';
 echo '<li><a href="http://hypertime.tk/findpswd.php">비번찾기</a></li>';
 }
 ?>
-</ul>
-</br>
+	</ul>
+	</br>
 <p>
 <img src="/images/welcome.png">
 </p>
