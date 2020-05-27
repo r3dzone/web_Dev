dcda53efd32b0467d74300db69161a05f7d8a35b
r3dzone <coolitop123@gmail.com>
2020-05-27 07:09:15 +0000

    add css file

--- web_site_ex/homepage/main.php
+++ web_site_ex/homepage/main.php
@@ -1,91 +1,19 @@
 <html>
 	<head>
-	<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
 		<title>정신과 시간의 방</title>
-		<style>
-		a{
-			color:black;
-			text-decoration: none;
-            }
-		ul#menu{
-			float:right;
-			padding: 0;
-		}
-		ul#menu li{
-			display:inline;
-		}
-		ul#menu li a{ 
-	    font-size:18px;
-            background-color: black;
-            color: white;
-			text-decoration: none;
-            }
-	ul#menu li a:hover {
-	background-color: white;
-	color: black;
-		}
-	
-		div{
-			margin: auto;
-		}		
-		table{
-			border: 0px; solid black;
-			text-align: center;
-		}
-		h1{
-		font-family: default;
-		}
-
-		body{
-		background-image:url('back_ground.png');
-		background-repeat:no-repeat;
-		background-position:50% 50%;
-		}
-		
-	#menubar {
-                        height: 40px;
-                        width: 1200px;
-                }
-
-                #menubar ul li {
-                        list-style: none;
-                        color: white;
-                        background-color: skyblue;
-                        float: left;
-                        line-height: 40px;
-                        vertical-align: middle;
-                        text-align: center;
-                }
-
-                #menubar .sel {
-                        text-decoration:none;
-                        color: white;
-                        display: block;
-                        width: 200px;
-                        font-size: 17px;
-                        font-weight: bold;
-                        font-family: "default";
-                }
-                #menubar .sel:hover {
-                        color: black;
-                        background-color: white;
-                }
-				
-				p{
-					text-align: center;
-				}
-				
-		 </style>
 	</head>
 	<body>
-	   <h1 font-family="ITCBLKAD"><a href="http://hypertime.tk/main.php">정신과 시간의 방</a>
+	   <h1 font-family="ITCBLKAD"><a href="/main.php"><img src="/images/logo.png" alt="정신과시간의방"></a>
 <nav id="menubar" >
 		<ul>
-			<li><a class="sel" href="http://hypertime.tk/freeboard.php">FreeBoard</a></li>
+			<li><a class="sel" href="/freeboard.php">FreeBoard</a></li>
 			<li>|</li>
-			<li><a class="sel" href="http://hypertime.tk/mypage.php">MyPage</a></li>
+			<li><a class="sel" href="/mypage.php">MyPage</a></li>
 			<li>|</li>
-			<li><a class="sel" href="">Secret</a></li>
+			<li><a class="sel" href="/chat.php">Chat</a></li>
 			<li>|</li>
 			<li><a class="sel" href="">Secret</a></li>
 			<li>|</li>
@@ -108,7 +36,7 @@ echo '<li><a href="http://hypertime.tk/findpswd.php">비번찾기</a></li>';
 </ul>
 </br>
 <p>
-<img src="welcome.png">
+<img src="/images/welcome.png">
 </p>
 </body>
 
